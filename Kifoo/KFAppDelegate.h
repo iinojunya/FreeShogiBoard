9a7bfdb000f1da3dcc631b088c25721a8a2c64bb
Maeda, Kazuya <kazuya.maeda@gmail.com>
2013-12-01 14:07:58 +0900

    Initial Commit

---
+++ Kifoo/KFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KFAppDelegate.h
+//  Kifoo
+//
+//  Created by Maeda Kazuya on 2013/12/01.
+//  Copyright (c) 2013年 Kifoo, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
